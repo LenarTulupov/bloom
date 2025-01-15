128c90335a0f77c4b8ba380231d388a0c5de7bab
Lenar <e6naa7981rrshift934a@gmail.com>
2025-01-15 22:20:58 +0400

    feature: added path to the user/sign-up page

--- src/client-pages/auth/user/sign-in/sign-in.tsx
+++ src/client-pages/auth/user/sign-in/sign-in.tsx
@@ -1,7 +1,14 @@
+import InputText from "@/components/ui/input-text/input-text";
+import Link from "next/link";
+
 export default function UserSignIn() {
   return (
     <div>
-      SignIn
+      <InputText placeholder="Email"/>
+      <InputText placeholder="Password"/>
+      <Link href='/user/sign-up'>
+        Register here
+      </Link>
     </div>
   )
 }
