a9f2f5e8240350184f5cbf9fc09d9202080a6102
Lenar <e6naa7981rrshift934a@gmail.com>
2025-01-26 19:23:28 +0400

    feature: eslint modification to get error when an import doesn't use

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,22 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["src/**/*.{js, ts, tsx}"],
+    plugins: ["unused-imports"],
+    rules: {
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_"
+        }
+      ]
+    }
+  }
 ];
 
 export default eslintConfig;
