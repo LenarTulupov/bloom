0feb99a27ae24814b33d4cf330ab5c4c36cc52db
lenar <e6naa7981rrshift934a@gmail.com>
2025-02-19 02:48:35 +0400

    fix: auth deleted files

--- src/client-pages/auth/admin/sign-in/sign-in-content/sign-in-form/sign-in-form.tsx
+++ src/client-pages/auth/admin/sign-in/sign-in-content/sign-in-form/sign-in-form.tsx
@@ -1,71 +1,69 @@
 "use client";
 
-import InputText from "@/components/ui/input-text/input-text";
-import Button from "@/components/ui/button/button";
-import { FormEvent, useState } from "react";
-import { URL } from "@/constants/url";
-import { API_ENDPOINTS } from "@/constants/api-endpoints";
-import Icon from "@/components/ui/icon/icon";
-import { ICONS } from "@/constants/icons";
+// import { FormEvent, useState } from "react";
+// import { URL } from "@/constants/url";
+// import { API_ENDPOINTS } from "@/constants/api-endpoints";
+// import Icon from "@/components/ui/icon/icon";
+// import { ICONS } from "@/constants/icons";
 import styles from "./sign-in-form.module.scss";
 
 export default function SignInForm() {
-  const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [error, setError] = useState<string>("");
-  const [loading, setLoading] = useState<boolean>(false);
-  const [showPassword, setShowPassword] = useState<boolean>(false);
+  // const [email, setEmail] = useState<string>("");
+  // const [password, setPassword] = useState<string>("");
+  // const [error, setError] = useState<string>("");
+  // const [loading, setLoading] = useState<boolean>(false);
+  // const [showPassword, setShowPassword] = useState<boolean>(false);
 
-  const toggleShowPassword = () => {
-    setShowPassword((p) => !p);
-  };
+  // const toggleShowPassword = () => {
+  //   setShowPassword((p) => !p);
+  // };
 
-  const handleSubmit = async (event: FormEvent) => {
-    event.preventDefault();
+  // const handleSubmit = async (event: FormEvent) => {
+  //   event.preventDefault();
 
-    setLoading(true);
-    setError("");
+  //   setLoading(true);
+  //   setError("");
 
-    try {
-      const response = await fetch(`${URL}${API_ENDPOINTS.ADMIN_LOGIN}`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ email, password }),
-      });
+  //   try {
+  //     const response = await fetch(`${URL}${API_ENDPOINTS.ADMIN_LOGIN}`, {
+  //       method: "POST",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ email, password }),
+  //     });
 
-      if (!response.ok) {
-        throw new Error("Invalid login credentials");
-      }
+  //     if (!response.ok) {
+  //       throw new Error("Invalid login credentials");
+  //     }
 
-      const data = await response.json();
-      const token = data.token;
+  //     const data = await response.json();
+  //     const token = data.token;
 
-      localStorage.setItem("adminToken", token);
+  //     localStorage.setItem("adminToken", token);
 
-      window.location.href = "/dashboard/admin";
-    } catch (error: unknown) {
-      if (error instanceof Error) {
-        setError("Failed to log in" + error.message);
-      } else {
-        setError("An unknown error occurred");
-      }
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     window.location.href = "/dashboard/admin";
+  //   } catch (error: unknown) {
+  //     if (error instanceof Error) {
+  //       setError("Failed to log in" + error.message);
+  //     } else {
+  //       setError("An unknown error occurred");
+  //     }
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
   return (
-    <form className={styles["admin-sign-in__form"]} onSubmit={handleSubmit}>
+    <form className={styles["admin-sign-in__form"]}>
       <div className={styles["admin-sign-in__form-email"]}>
         <label htmlFor="email">Email</label>
-        <InputText
+        {/* <InputText
           id="email"
           type="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
-        />
+        /> */}
       </div>
       <div
         className={`
@@ -75,27 +73,27 @@ export default function SignInForm() {
       >
         <label htmlFor="password">Password</label>
         <div className={styles["form-password__field"]}>
-          <InputText
+          {/* <InputText
             className={styles["form-password__field-input"]}
             id="password"
             type={showPassword ? "text" : "password"}
             value={password}
             onChange={(e) => setPassword(e.target.value)}
             required
-          />
-          <button
+          /> */}
+          {/* <button
             className={styles["form-password__field-button"]}
             onClick={toggleShowPassword}
             type="button"
           >
             <Icon icon={showPassword ? ICONS.hide : ICONS.show} color="gray" />
-          </button>
+          </button> */}
         </div>
       </div>
-      {error && <div style={{ color: "red" }}>{error}</div>}
-      <Button type="submit" disabled={loading}>
+      {/* {error && <div style={{ color: "red" }}>{error}</div>} */}
+      {/* <Button type="submit" disabled={loading}>
         {loading ? "Logging in..." : "Sign In"}
-      </Button>
+      </Button> */}
     </form>
   );
 }
--- src/client-pages/auth/user/sign-in/sign-in.tsx
+++ src/client-pages/auth/user/sign-in/sign-in.tsx
@@ -1,11 +1,10 @@
-import InputText from "@/components/ui/input-text/input-text";
 import Link from "next/link";
 
 export default function UserSignIn() {
   return (
     <div>
-      <InputText placeholder="Email"/>
-      <InputText placeholder="Password"/>
+      {/* <InputText placeholder="Email"/>
+      <InputText placeholder="Password"/> */}
       <Link href='/user/sign-up'>
         Register here
       </Link>
