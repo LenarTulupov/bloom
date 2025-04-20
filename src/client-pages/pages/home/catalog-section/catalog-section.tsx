e1d5b98d97412d544f1e3571b2e0e94d99173b40
lenar <e6naa7981rrshift934a@gmail.com>
2025-04-20 19:30:20 +0400

    fix: dropdown panel 3

--- src/client-pages/pages/home/catalog-section/catalog-section.tsx
+++ src/client-pages/pages/home/catalog-section/catalog-section.tsx
@@ -4,31 +4,33 @@ import ProductsGrid from "@/components/ui/products-grid/products-grid";
 import Title from "@/components/ui/title/title";
 import { navItemsLeft } from "@/constants/nav-items";
 import React from "react";
-import styles from './catalog-section.module.scss'
+import styles from "./catalog-section.module.scss";
 import Link from "next/link";
 
 export default function CatalogSection() {
   return (
     <section>
       <Container>
-        <div className={styles['catalog-section__content']}>
-          <div className={styles['catalog-section__text']}>
+        <div className={styles["catalog-section__content"]}>
+          <div className={styles["catalog-section__text"]}>
             <Title>Catalog</Title>
             <Link href="/catalog">
-            <Title>View All</Title>
+              <Title>View All</Title>
             </Link>
           </div>
           <ProductsGrid>
             {navItemsLeft.map((item) =>
-              item.subItems
+              item.categories
                 ?.slice(0, 5)
-                .map((subItem) => (
-                  <CatalogCard
-                    key={subItem.id}
-                    name={subItem.name}
-                    link={subItem.href}
-                  />
-                ))
+                .map((category) =>
+                  category.href ? (
+                    <CatalogCard
+                      key={category.id}
+                      name={category.category}
+                      link={category.href}
+                    />
+                  ) : null
+                )
             )}
           </ProductsGrid>
         </div>
